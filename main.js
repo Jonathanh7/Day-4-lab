19dca7f01972ca332afccf144227c15231bebfb2
Jonathan Hernandez <jonathanhernandez@Jonathans-MBP.lan>
2022-01-21 10:47:23 -0500

    created lab

---
+++ main.js
@@ -0,0 +1,74 @@
+
+
+//Exercise #1
+//Write a while loop that starts counting from 5.
+//Count up to (including) 50 and count only every fifth number
+//Don' forget to log it
+
+
+//Exercise #2
+//Write a function that takes an array of numbers as an argument.
+//This function should return the first duplicate number in the array:
+// For example: [2,7,5,9,8,7] should return 7;
+
+
+
+//Exercise #3
+//Write a function that loops over the following array of TKH students
+// and prints out their name and what class they are currently in.
+// example => Ajitha is in class 2022-01-RTT 
+const students = [
+  "Ajitha",
+  "Amari",
+  "Anne",
+  "Aylin",
+  "Bosen",
+  "Charlton",
+  "Demmy",
+  "Godbless",
+  "Hee",
+  "Janet",
+  "Junior",
+  "Lance",
+  "Nyasho",
+  "Poojitha",
+  "Rebbecca",
+  "Saif",
+  "Stephanus",
+  "Vedha"
+];
+
+
+
+
+
+//Exercise #4
+//http://math2.org/math/geometry/circles.htm
+//
+//Create 2 functions that calculate properties of a circle:
+//First: Create a function called calcCircumference
+// - Pass the radius to the function
+// - Calculate the circumference based on the radius
+// - and output " The circumference is SOMETHING"
+// Second: Create a function called calcArea:
+// - Pass the radius to the function
+// - Calculate the area based on the radius and output "The area is SOMETHING"
+
+
+
+//Exercise #5
+//Create a function called mixUp
+//It should take in two stings, and return the concatenation of the two strings(separated by a space)
+//slicing out and swapping the first 2 characters of each. You can assume that the strings are at least 2 characters long. 
+//For example:
+//mixUp('mix', 'pod'): 'pox mid'
+//mixUp('dog', 'dinner'): 'dig donner'
+//write answer below 
+
+
+
+
+//Exercise #6
+//Write a function that takes 3 parameters and returns one number, which is
+//the product of the first two numbers raised to the power of the third
+//passing this function 1,2,3 should give you back the answer to (1 * 2)^3
